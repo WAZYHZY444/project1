a386ba38ab57f51a43b9464a594b102290bc5cc2
Bird-ZY <3270275674@qq.com>
2025-12-16 20:37:08 +0800

    笔试题5.c

---
+++ 笔试题5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a[5][5];
+	int (*p)[4];
+	p=a;
+	printf("%p %d\n",&p[4][2]-&a[4][2],&p[4][2]-&a[4][2]);
+	return 0;
+}
+//a->int(*)[5]
+//b->int(*)[4]
+//p=a  等号两边类型不相等，所以会报警告
