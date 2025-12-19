e7bcc770301b0595ce79062098873b84b598bc07
Bird-ZY <3270275674@qq.com>
2025-12-19 10:01:42 +0800

    experiments 6_1(链表).c(代码优化）

--- experiments 6_1(链表).c
+++ experiments 6_1(链表).c
@@ -10,14 +10,20 @@ typedef struct node
 //打印链表
 void printList(LinkNode* head)
 {
-	LinkNode* p; //访问指针
-	if(head->next==NULL)return; //如果是空链表，不打印
-	p=head->next; //指向头节点的后继节点
+//	LinkNode* p; //访问指针
+//	if(head->next==NULL)return; //如果是空链表，不打印
+//	p=head->next; //指向头节点的后继节点
+//	while(p!=NULL){
+//		printf("%d ",p->data);
+//		p=p->next;
+//	}
+//	return;
+
+	LinkNode* p=head->next; //因为头节点的数据域为空，所以跳过头节点，遍历不经过头节点
 	while(p!=NULL){
 		printf("%d ",p->data);
 		p=p->next;
 	}
-	return;
 }
 //销毁链表
 bool destroyList(LinkNode* head)
@@ -53,45 +59,56 @@ int main()
 	p->next=NULL;//用尾插法，新插入的节点始终是尾节点，尾节点的指针域始终为NULL
 //r指针指向尾节点
 	r->next=p;
+//更新尾部指针指向
 	r=p;
-	//r=r->next;
 	}
-	scanf("%d",&num); //可以不重新定义一个变量
-	r=head; //r指向的是p访问当前节点的前驱节点
-	p=head->next; //p访问当前节点
-	while(p->next!=NULL){
-		//匹配
+	
+	
+//	scanf("%d",&num); //可以不重新定义一个变量
+//	r=head; //r指向的是p访问当前节点的前驱节点
+//	p=head->next; //p访问当前节点
+//	while(p->next!=NULL){
+//		//匹配
+//		if(p->data==num){
+//			r->next=p->next; //删除
+//			free(p);         //释放
+//			p=r->next;
+//		}else{
+//			r=p;
+//			p=p->next;
+//		}
+//
+//	}
+////处理尾节点
+//	if(p->data==num){
+//		r->next=NULL;
+//		free(p);
+//	}
+
+
+	scanf("%d",&num);
+	r=head;
+	p=r->next;
+	while(p!=NULL){
 		if(p->data==num){
-			r->next=p->next; //删除
-			free(p);         //释放
+			//删除节点
+			r->next=p->next;
+			LinkNode* temp=p;
+			free(temp);
+//free(pCurrent),之后又将 pCurrent = pPrev->next;
+//导致释放的是已更新的指针，可能会访问未初始化或已释放的内存,所以需要创建一个临时指针temp来保存p
+			//p移动到下一个节点
 			p=r->next;
 		}else{
+			//当前节点不删除，移动两个辅助指针
 			r=p;
 			p=p->next;
 		}
-		
 	}
-//处理尾节点
-	if(p->data==num){
-		r->next=NULL;
-		free(p);
-	}
+
+
 	printList(head);
 	destroyList(head);
 	return 0;
 }
 //在前面的for循环结束后，r,p,num等的作用释放出来，所以可以不需要重定义新的变量
-
-/*
-while(strcmp(scanf("%s",sID),"end")!=0){  //用学号来比较
-	scanf("%d ",&num);  //6
-//创建新节点
-	p=(LinkNode*)malloc(sizeof(LinkNode));
-	p->data=num;
-	p->next=NULL;//用尾插法，新插入的节点始终是尾节点，尾节点的指针域始终为NULL
-//r指针指向尾节点
-	r->next=p;
-	//r=p;
-	r=r->next;
-}
-*/
