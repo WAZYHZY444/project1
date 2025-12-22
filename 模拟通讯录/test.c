91e4da4adf2b313b827c2226a227e872bf371040
Bird-ZY <3270275674@qq.com>
2025-12-22 09:50:36 +0800

    模拟通讯录/test.c(添加枚举增加代码可读性)

--- 模拟通讯录/test.c
+++ 模拟通讯录/test.c
@@ -1,5 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "contact.h"
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
+
 void menu()
 {
 	printf("************************************************\n");
@@ -22,25 +34,25 @@ int main()
 		scanf("%d", &input);
 		switch (input)
 		{
-			case 1:
+			case ADD:
 				AddContact(&con);
 				break;
-			case 2:
+			case DEL:
 				DelContact(&con);
 				break;
-			case 3:
+			case SEARCH:
 				SearchContact(&con);
 				break;
-			case 4:
+			case MODIFY:
 				ModifyContact(&con);
 				break;
-			case 5:
+			case SHOW:
 				ShowContact(&con);
 				break;
-			case 6:
+			case SORT:
 				SortContact(&con);
 				break;
-			case 0:
+			case EXIT:
 				printf("exit\n");
 				break;
 				default:
