20e67a422313360bdaa992004d45e5415427fc03
Bird-ZY <3270275674@qq.com>
2025-12-24 23:17:49 +0800

    sscanf、sprintf.c(代码补充)

--- sscanf、sprintf.c
+++ sscanf、sprintf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int main() 
 {
     char buffer[100];
@@ -21,3 +21,49 @@ int main()
     
     return 0;
 }
+*/
+
+//sscanf:是把一个字符串转化成一个格式化数据
+//sprintf:是把一个格式化的数据转化成字符串
+struct S
+{
+	char arr[10];
+	int age;
+	float score;
+};
+int main()
+{
+	struct S s={"zhangyuan",40,99.9f};
+	struct S temp={0};
+	char buf[100]={0};
+	//把s中的格式化数据转化成字符串放到buf中
+	sprintf(buf,"%s %d %f",s.arr,s.age,s.score);
+	printf("字符串：%s\n",buf);
+	
+	//从字符串buf中获取一个格式化数据到temp中
+	sscanf(buf,"%s %d %f",temp.arr,&(temp.age),&(temp.score));
+	printf("格式化：%s %d %f\n",temp.arr,temp.age,temp.score);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
