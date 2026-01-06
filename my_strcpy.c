3661c0cd605407b018c14bcbedb0408a6b20bba2
Bird-ZY <3270275674@qq.com>
2026-01-06 14:46:03 +0800

    添加注释

--- my_strcpy.c
+++ my_strcpy.c
@@ -22,7 +22,7 @@ int main()
 
 char* my_strcpy(char* dest,const char* src)
 {
-	char* ret=dest;
+	char* ret=dest;   //保存原始目标的地址(数组的首地址)，便于正确返回,dest指针在后面发生了移动，所指向的位置发生了改变
 	if(dest!=NULL&&src!=NULL){
 		while((*dest++ = *src++))  //当*src为'\0'时，'\0'赋值给*dest，'\0'的ASCII码值为0，表达式的结果为0，为假，循环停下来
 		{
