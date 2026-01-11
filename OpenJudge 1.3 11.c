01121c2adf52172a1e9b010bad5a80f5c7bc4066
Bird-ZY <3270275674@qq.com>
2026-01-11 16:36:55 +0800

    添加注释

--- OpenJudge 1.3 11.c
+++ OpenJudge 1.3 11.c
@@ -4,7 +4,7 @@ int main()
 	double a,b;
 	scanf("%lf %lf",&a,&b);
 	double y=a/b;
-	long x=(long)y;
+	long x=(long)y;  //将浮点型数据转化为整形数据，会截断小数点部分，只保留整数部分
 	double T=a-(x*b);
 	printf("%g",T);
 	return 0;
