4489cded388474fb8a4b60450c81e61481cf0c53
Bird-ZY <3270275674@qq.com>
2026-01-30 11:20:08 +0800

    求子列的最大值(2)(算法优化)

--- 算法优化.c
+++ 算法优化.c
@@ -1,6 +1,6 @@
 //求子列的最大值
 
-//算法一：
+//算法一：  T(N)=O(n^3)
 int MaxSubseqSum1(int A[],int N)
 {
 	int ThisSum,MaxSum=0;
@@ -12,6 +12,21 @@ int MaxSubseqSum1(int A[],int N)
 				if(ThisSum>MaxSum){
 					MaxSum=ThisSum;
 				}
+			}
+		}
+	}
+	return MaxSum;
+}
+
+//算法二： T(N)=O(n^2)
+int MaxSubseqSum2(int A[],int N)
+{
+	int ThisSum,MaxSum=0;
+	for(int i=0;i<N;i++){
+		for(int j=i;j<N;j++){
+			ThisSum+=A[j];
+			if(ThisSum>MaxSum){
+				MaxSum=ThisSum;
 			}
 		}
 	}
