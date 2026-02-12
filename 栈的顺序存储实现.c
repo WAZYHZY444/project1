037c9638fc6f145309b6be1f9fb6fc685ab89b20
Bird-ZY <3270275674@qq.com>
2026-02-12 11:15:50 +0800

    栈的顺序存储实现.c

---
+++ 栈的顺序存储实现.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAXSIZE 10
+
+typedef struct
+{
+	ElemType data[MAXSIZE];
+	int top;  //计数
+}SqStack;
+
+//栈的初始化
+void InitStack(SqStack* S)
+{
+	S.top=-1;
+}
+
+bool StackEmpty(SqStack S)
+{
+	if(S.top==-1){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+//新元素压栈
+bool Push(SqStack* S,ElemType x)
+{
+	if(S.top==MAXSIZE-1){
+		return false;
+	}
+	S.top=S.top+1;
+	S.data[S.top]=x;
+	//S.data[++S.top]=x;
+	return true;
+}
+
+//获取栈顶元素值
+ElemType Push(SqStack* S)
+{
+	if(S.top==MAXSIZE-1){
+		return 0;
+	}
+	int x=S.data[S.top];
+	return x;
+}
+
+//出栈
+ElemType Pop(SqStack* S) //数据还残留在内存中，只是逻辑上删除了
+{
+	if(S.top==MAXSIZE-1){
+		return 0;
+	}
+	ElemType x=S.data[S.top];
+	S.top=S.top-1;
+	//int x=S.data[S.top--];
+	return x;
+}
+
+void teskStack()
+{
+	SqStack S;  //声明一个顺序栈(分配空间)
+	InitStack(&S);
+	
+}
