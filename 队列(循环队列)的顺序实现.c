e1b67dfb73c0973614a608845dc6f8f74c403076
Bird-ZY <3270275674@qq.com>
2026-02-13 11:16:08 +0800

    队列(循环队列)的顺序实现.c

---
+++ 队列(循环队列)的顺序实现.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAXSIZE 10
+
+typedef struct
+{
+	ElemType data[MAXSIZE];
+	int front;   //队头指针，指向队头元素
+	int rear;    //队尾指针，指向队尾元素的后一个位置(下一个元素插入的位置)
+}SqQueue;
+
+//队列元素个数：(rear+MAXSIZE-front)%MAXSIZE
+
+//队列的初始化
+void InitQueue(SqQueue* Q)
+{
+	Q->front=0;
+	Q->rear=0;
+}
+
+//判断队列是否为空
+bool isEmpty(SqQueue* Q)
+{
+	return Q->front==Q->rear;
+}
+
+//判断队列是否已满
+bool isFull(SqQueue* Q)
+{
+	return (Q->rear+1)%MAXSIZE==Q->front;  //循环队列(会浪费一个ElemType类型的空间)
+}
+
+//入队
+bool EnQueue(SqQueue* Q,ElemType x)
+{
+	if(isFull(Q)){
+		return false;
+	}
+	Q->data[Q->rear]=x;
+	Q.rear=(Q.rear+1)%MAXSIZE;
+	return true;
+}
+
+//出队
+bool DeQueue(SqQueue* Q,ElemType* x)
+{
+	if(isEmpty(Q)){
+		return false;
+	}
+	*x=Q->data[Q->front];
+	Q->front=(Q->front+1)%MAXSIZE;
+	return true;
+}
+
+//获取队头元素
+bool DeQueue(SqQueue* Q,ElemType* x)
+{
+	if(isEmpty(Q)){
+		return false;
+	}
+	*x=Q->data[Q->front];
+	return true;
+}
