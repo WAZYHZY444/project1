615bd907415f37357d5927a79057d89004a0e38d
Bird-ZY <3270275674@qq.com>
2026-02-25 15:37:41 +0800

    二叉树线索化(先序线索化)

--- 二叉树线索化.c
+++ 二叉树线索化.c
@@ -11,6 +11,8 @@ typedef struct ThreadNode
 //全局变量
 ThreadNode* pre=NULL;
 
+/*中序线索化*/
+
 //中序遍历二叉树(边遍历，边线索化)
 void InThread(ThreadNode* T)
 {
@@ -47,3 +49,43 @@ void CreateInThread(ThreadNode* T)
 }
 //问：ThreadNode* pre=NULL，pre不是一个结构体指针吗，又不是节点，为什么可以用成员访问运算符->
 //答：pre=q; 这行代码执行后，pre就不再是NULL，而是变成了指向实际节点的指针，所以后续的调用中就可以使用 pre-> 来访问这个节点的成员了。
+
+
+/*先序线索化*/
+
+//先序遍历二叉树(边遍历，边线索化)
+void PreThread(ThreadNode* T)
+{
+	if(T!=NULL){
+		Visit(T);
+		if(T->ltag==0){    //避免出现循环线索化(只有先序遍历会出现循环)
+			PreThread(T->LeftChild);
+		}
+		PreThread(T->RightChild);
+	}
+}
+
+void Visit(ThreadNode* q)
+{
+	if(q->LeftChild==NULL){
+		q->LeftChild=pre;
+		q->ltag=1;
+	}
+	if(pre!=NULL&&q->RightChild==NULL){
+		pre->RightChild=q;
+		pre->rtag=1;
+	}
+	pre=q;
+}
+
+//中序线索化二叉树T
+void CreateInThread(ThreadNode* T)
+{
+	pre=NULL;
+	if(T!=NULL){
+		PreThread(T);           //中序线索化二叉树
+		if(pre->RightChild==NULL){
+			pre->rtag=1;       //处理遍历的最后一个节点
+		}
+	}
+}
