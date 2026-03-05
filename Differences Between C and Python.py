3f5a8e7e6927f76ed33d00da052aeece0e7f48d5
Bird-ZY <3270275674@qq.com>
2026-03-05 18:00:03 +0800

    Differences Between C and Python.py (C语言与Python的不同的点)

---
+++ Differences Between C and Python.py
@@ -0,0 +1,41 @@
+# Python与C语言重要区别
+
+#1.
+# 在Python中使用算数运算符/，结果一定是浮点数 (在python中，使用算数运算符，操作数有浮点数，返回值一定是浮点数)
+print(25/5)
+print(25//5) #返回整数(向下取整)
+print(-7//2)
+
+#2.
+# Python中的取模运算符%，两个操作数既可以是整数(包括负数)，也可以是浮点数
+print(10.5%3)
+
+#3.
+# 幂运算，Python使用**运算符(但也可以使用pow函数)，C语言运用pow函数
+print(2**3)
+
+#4.
+# Python中print输出末尾默认换行，C语言换行需要手动加\n
+
+#5.
+# C语言：&&   ||   !
+# python：and  or  not
+# print(not 3>9)
+
+#6.
+# 三目运算符
+# c         condition ? expression1 : expression2;      a>b?a:b
+# python    expression1 if condition else expression2   a if a>b else b
+
+#7.
+# C语言  if-if else-else
+#python  if-elif-else
+
+#8.
+# Python没有C语言那样的++和--自增/自减运算符
+
+#9.
+# 强制类型转换时，C语言：数据类型加括号,(int)x; python：变量名加括号,int(x).
+
+#10.
+#isupper/islower函数. C语言：处理的是单个字符，返回0或非0; python：处理的是整个字符串，返回True或False。
