7f8cb3dc92bb76992140237a7a63d8a7d8793b42
Bird-ZY <3270275674@qq.com>
2026-03-08 17:00:06 +0800

    并查集进一步优化(压缩路径)

--- 并查集(双亲表示法思想).c
+++ 并查集(双亲表示法思想).c
@@ -44,5 +44,20 @@ void Union(int S[],int Root1,int Root2)
 	}
 }
 
-//优化之前，Find函数的最坏时间复杂度为O(n),优化之后，最坏时间复杂度为O(log2(n))
+//“并”操作优化之前，Find函数的最坏时间复杂度为O(n),优化之后，最坏时间复杂度为O(log2(n))
 //Union函数时间复杂度始终为O(1)
+
+//“查”操作优化，先找到根节点，再进行“压缩路径”
+int Find(int [],int x)
+{
+	int root=x;
+	while(root>=0) root=S[root];   //循环找到根节点
+	while(root!=x){
+		int t=S[x];
+		S[x]=root;   //下标为x的节点直接挂到根节点下面
+		x=t;         //不能直接写x=S[x];因为S[x]=root改变了S[x]的值
+	}
+	return root;
+}
+
+//“查”操作优化后，可使根的高度不超过O(α(n)).α(n)是一个增长缓慢的函数，对于常见的n值，通常α(n)<=4,因此Find优化后并查集的find、Union操作时间开销都很低
