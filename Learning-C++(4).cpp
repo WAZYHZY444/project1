0bb629759fc13101312282696b9d9d0cfe268b28
Bird-ZY <3270275674@qq.com>
2026-04-06 14:50:31 +0800

    C++学习 4-6(3)

--- Learning-C++(4).cpp
+++ Learning-C++(4).cpp
@@ -2,6 +2,7 @@
 //STL六大组件：容器、算法、迭代器、仿函数、适配器（配接器）、空间配置器
 
 #include <iostream>
+#include <string>
 using namespace std;
 #include <algorithm>  //标准算法头文件
 //容器vector   算法for_each   迭代器vector<int>::iterator
@@ -10,7 +11,7 @@ using namespace std;
 void myPrint(int val){
 	cout<<val<<endl;
 }
-//vector容器存放内置数据类型
+                        //****vector容器存放内置数据类型****//
 void test01(){
 	//创建一个vector容器
 	vector<int> v;
@@ -38,8 +39,76 @@ void test01(){
 	for_each(v.begin(),v.end(),myPrint);
 }
 
+					//****vector容器中存放自定义数据类型 类****//
+class Person{
+public:
+	string name;
+	int age;
+	
+	Person(string m_name,int m_age){
+		name=m_name;
+		age=m_age;
+	}
+};
+	
+void test02(){
+	vector<Person> v;
+	Person p1("ZhangYuan",40);
+	Person p2("SuXing",41);
+	Person p3("LuHu",39);
+	//向容器中添加数据
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	//遍历容器中的数据
+	for(vector<Person>::iterator it=v.begin();it!=v.end();it++){
+		cout<<"姓名："<<it->name<<"年龄："<<it->age<<endl;
+	}
+}
+
+				//****vector容器中存放自定义数据类型 指针****//
+void test03(){
+	vector<Person*> v;
+	Person p1("ZhangYuan",40);
+	Person p2("SuXing",41);
+	Person p3("LuHu",39);
+	//向容器中添加数据
+	v.push_back(&p1);
+	v.push_back(&p2);
+	v.push_back(&p3);
+	//遍历容器中的数据
+	for(vector<Person*>::iterator it=v.begin();it!=v.end();it++){
+		cout<<"姓名："<<(*it)->name<<"年龄："<<(*it)->age<<endl;
+	}
+}			
+
+				//****vector容器嵌套vector容器****//
+void test04(){
+	vector<vector<int>> v;
+	//创建小容器
+	vector<int> v1;
+	vector<int> v2;
+	vector<int> v3;
+	//向小容器添加数据
+	for(int i =0;i<3;i++){
+		v1.push_back(i+1);
+		v2.push_back(i+2);
+		v3.push_back(i+3);
+	}
+	//将小容器插入到大容器中
+	v.push_back(v1);
+	v.push_back(v2);
+	v.push_back(v3);
+	//通过大容器，把所有数据遍历一遍
+	for(vector<vector<int>>::iterator it=v.begin();it!=v.end();it++){
+		for(vector<int>::iterator vit=(*it).begin();vit!=(*it).end();vit++){
+			cout<<*vit<<" ";
+		}
+		cout<<endl;
+	}
+}
 int main()
 {
-	test01();
+	test04();
 	return 0;
 }
