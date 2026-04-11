f3aaca59aebc72fd24bfb3d9206d56e7e497645d
Bird-ZY <3270275674@qq.com>
2026-04-11 21:54:01 +0800

    朴素模式匹配算法(2).c（代码修正）

--- 朴素模式匹配算法(2).c
+++ 朴素模式匹配算法(2).c
@@ -1,28 +1,34 @@
 #include <stdio.h>
-#define MAXLEN 255
+#include <string.h>
 
-typedef struct
+int Match(const char* text,const char* pattern)
 {
-	char ch[MAXLEN];
-	int length;
-}String;
-
-int Match(String S,String T)
-{
-	int i=1,j=1;    //跳过下标为0
-	while(i<=S.length&&j<=T.length){
-		if(S.ch[i]==T.ch[j]){
+	int len1=strlen(text);
+	int len2=strlen(pattern);
+	int i=0,j=0;
+	while(i<len1&&j<len2){
+		if(pattern[j]==text[i]){
 			i++;
 			j++;
-		}else{
-			i=i-j+2;  //i指针回溯
-			j=1;
 		}
+		else{
+			i=i-j+1;   //i指针回溯
+			j=0;
+		}	
 	}
-	if(j>T.length){
-		return i-T.length;
-	}else{     //模式串与母串完全不匹配
+	if(j>=len2)
+		return i-len2;
+	else
 		return 0;
-	}
 }
+
+int main()
+{
+	const char* text="abcdefghcdefgh";  //母串
+	const char* pattern="efg";    //模式串
+	int  result=Match(text,pattern);
+	printf("%d\n",result+1);
+	return 0;
+}
+
 //最坏时间复杂度：O(mn)
