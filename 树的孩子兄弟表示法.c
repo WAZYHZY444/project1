f898bf6a577e3b011a0c4cb152c2dab52818b154
Bird-ZY <3270275674@qq.com>
2026-04-22 20:42:45 +0800

    树的孩子兄弟表示法.c

--- 树的孩子兄弟表示法.c
+++ 树的孩子兄弟表示法.c
@@ -16,10 +16,59 @@ CSNode* createNode(char val)
 	newnode->nextsibing=NULL;
 	return newnode;
 }
+//原树：
+//        A
+//      / | \
+//     B  C  D
+//    / \    |
+//   E   F   G
 
-void createTree(CSNode** T)
+CSNode* createTree()
 {
-	*T=createNode('A');
-	(*T)->firstchild=createNode('B');
-	(*T)->
+	CSNode* A=createNode('A');
+	CSNode* B=createNode('B');
+	CSNode* C=createNode('C');
+	CSNode* D=createNode('D');
+	CSNode* E=createNode('E');
+	CSNode* F=createNode('F');
+	CSNode* G=createNode('G');
+	
+	A->firstchild=B;
+	B->firstchild=E;
+	B->nextsibing=C;
+	C->nextsibing=D;
+	E->nextsibing=F;
+	D->firstchild=G;
+	
+	return A;
+}
+
+//先根遍历
+void preOrder(CSNode* T)
+{
+	if(T!=NULL){
+		printf("%c ",T->data);
+		preOrder(T->firstchild);
+		preOrder(T->nextsibing);
+	}
+}
+
+//释放内存（后序遍历）
+void DestroyByPostOrder(CSNode* T)
+{
+	if(T!=NULL){
+		DestroyByPostOrder(T->firstchild);
+		DestroyByPostOrder(T->nextsibing);
+		free(T);
+	}
+}
+
+int main()
+{
+	CSNode* root=createTree();
+	preOrder(root);
+	DestroyByPostOrder(root);
+	root=NULL;
+	
+	return 0;
 }
