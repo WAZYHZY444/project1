ab954e3b3e7745374481b66a60fa2a4820c7456c
Bird-ZY <3270275674@qq.com>
2026-05-28 19:31:44 +0800

    纠正代码

--- 排序.c
+++ 排序.c
@@ -1,10 +1,11 @@
 //直接插入排序
 void InsertSort(int arr[],int n)
 {
-	for(int i=1;i<n;i++){
+	int i,j,temp;
+	for(i=1;i<n;i++){
 		if(arr[i]<arr[i-1]){
-			int temp=arr[i];
-			for(int j=i-1;j>=0;a[j]>temp&&j--){
+			temp=arr[i];
+			for(j=i-1;j>=0&&arr[j]>temp;j--){
 				arr[j+1]=arr[j];
 			}
 			arr[j+1]=temp;
@@ -13,12 +14,13 @@ void InsertSort(int arr[],int n)
 }
 
 //直接插入排序（带哨兵）
-void InsertSqort(int arr[],int n)
+void InsertSort(int arr[],int n)
 {
-	for(int i=2;i<=n;i++){
+	int i,j;
+	for(i=2;i<=n;i++){
 		if(arr[i]<arr[i-1]){
 			arr[0]=arr[i];
-			for(int j=i-1;arr[0]<arr[j];j--){
+			for(j=i-1;arr[0]<arr[j];j--){
 				arr[j+1]=arr[j];
 			}
 		}
